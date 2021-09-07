5a8aaa2907784b99e8ea7fe5ef070caca6f3eac8
Andrew Spittlemeister <spittlemeister@brsc.com>
2021-09-07 17:29:16 -0500

    added data types + build AST for described protocol

--- bytechomp/datatypes.py
+++ bytechomp/datatypes.py
@@ -1,1 +1,79 @@
 # use typing module to create new datatypes that can be referenced for parsing. Make sure derivative types inherit the corresponding type in python (i.e. u8 inherits int so that it can be used in the dataclass after parsing as an int).
+# make mapping of new types to struct codes and also make that mapping FINAL
+
+from typing import NewType, Final, Union
+
+
+### Elementary Data Types #############################################################################################
+PAD = NewType("PAD", int)
+U8 = NewType("U8", int)
+U16 = NewType("U16", int)
+U32 = NewType("U32", int)
+U64 = NewType("U64", int)
+I8 = NewType("I8", int)
+I16 = NewType("I16", int)
+I32 = NewType("I32", int)
+I64 = NewType("I64", int)
+F16 = NewType("F16", float)
+F32 = NewType("F32", float)
+F64 = NewType("F64", float)
+
+ELEMENTARY_TYPE = Union[PAD, U8, U16, U32, U64, I8, I16, I32, I64, F16, F32, F64, int, float]
+ELEMENTARY_TYPE_LIST = [PAD, U8, U16, U32, U64, I8, I16, I32, I64, F16, F32, F64, int, float]
+#######################################################################################################################
+
+### String-like Data Types ############################################################################################
+STRING = NewType("STRING", str)
+BYTES = NewType("BYTES", bytes)
+#######################################################################################################################
+
+TYPE_TO_TAG: dict[ELEMENTARY_TYPE, str] = {
+    PAD: "x",
+    U8: "B",
+    U16: "H",
+    U32: "I",
+    U64: "Q",
+    I8: "b",
+    I16: "h",
+    I32: "i",
+    I64: "q",
+    F16: "e",
+    F32: "f",
+    F64: "d",
+    int: "Q",
+    float: "d"
+}
+
+TYPE_TO_PYTYPE: dict[ELEMENTARY_TYPE, type] = {
+    PAD: None,
+    U8: int,
+    U16: int,
+    U32: int,
+    U64: int,
+    I8: int,
+    I16: int,
+    I32: int,
+    I64: int,
+    F16: float,
+    F32: float,
+    F64: float,
+    int: float,
+    float: float
+}
+
+TYPE_TO_LENGTH: dict[ELEMENTARY_TYPE, int] = {
+    PAD: 1,
+    U8: 1,
+    U16: 2,
+    U32: 4,
+    U64: 8,
+    I8: 1,
+    I16: 2,
+    I32: 4,
+    I64: 8,
+    F16: 2,
+    F32: 4,
+    F64: 8,
+    int: 8,
+    float: 8
+}
--- bytechomp/reader.py
+++ bytechomp/reader.py
@@ -1,44 +1,134 @@
 from __future__ import annotations
-from typing import TypeVar, Generic
-from dataclasses import is_dataclass, fields
+from typing import Any, Union, Annotated, get_origin, get_args, GenericAlias
+from dataclasses import dataclass, is_dataclass, fields, MISSING
+from collections import OrderedDict
 import inspect
 
-# NOTE: can't seem to get the instantiation of T from within the class, may need to revert to variable or look into abstract base classes.
-T = TypeVar("T")
-class Reader(Generic[T]):
-    def __init__(self, datatype: type) -> None:
-        # for member in inspect.getmembers(self):
-        #     print(member)
+from bytechomp.datatypes import *
+
+
+@dataclass
+class BasicProtocolSchema:
+    parsing_type: Union[ELEMENTARY_TYPE, STRING, BYTES]
+    python_type: Union[int, float, str, bytes]
+    parser_tag: str
+    length: int
+    default_value: Union[int, float, str, bytes, None] = None
+    raw_data: bytes = b""
+    parsed_value: Union[int, float, str, bytes, None] = None
+
+    
+class Reader:
+    def __init__(self, datatype: type, byte_order: str = "little") -> None:
         if not inspect.isclass(datatype) or not is_dataclass(datatype):
             raise Exception("datatype must be a dataclass declaration")
 
-        self.__datatype: type = datatype
+        if byte_order not in ["big", "little"]:
+            raise Exception("byte order must be one \"big\" or \"little\"")
+
+        self.__datatype = datatype
+        self.__byte_order = byte_order
+        self.__is_complete = False
+        self.__data = b""
 
         # verify that the datatype contains only known types
-        self.__object_description: list = self.__build_nested_data_description(datatype)
+        self.__data_description: OrderedDict = self.__build_data_description(self.__datatype)
 
-    def __build_nested_data_description(self, datatype: type) -> list:
-        object_description: list = []
+        print(self.__data_description)
+
+    def __build_data_description(self, datatype: type) -> OrderedDict:
+        object_description: dict[str, Union[list, dict, ELEMENTARY_TYPE]] = OrderedDict()
 
         for field in fields(datatype):
-            # if field is a dataclass, recurse
-            if is_dataclass(field.type):
-                object_description.append(
-                    {
-                        "name": field.name,
-                        "is_anchor": False,
-                    }
+            if field.type in ELEMENTARY_TYPE_LIST:
+                object_description[field.name] = BasicProtocolSchema(
+                    parsing_type=field.type,
+                    python_type=TYPE_TO_PYTYPE[field.type],
+                    parser_tag=TYPE_TO_TAG[field.type],
+                    length=TYPE_TO_LENGTH[field.type],
+                    default_value=None if field.default == MISSING else field.default,
                 )
+            elif inspect.isclass(field.type) and is_dataclass(field.type):
+                if field.default != MISSING:
+                    raise Exception(f"cannot have default value on nested types (field: {field.name})")
+                object_description[field.name] = self.__build_data_description(field.type)
+            elif get_origin(field.type) == Annotated:
+                args = get_args(field.type)
+
+                if len(args) != 2:
+                    raise Exception(f"annotated value should only have two arguments (field: {field.name})")
+
+                arg_type = args[0]
+                length = args[1]
+
+                if type(length) != int:
+                    raise Exception(f"second annotated argument must be an integer to denote length")
+
+                # deal with string type
+                if arg_type in [STRING, str]:
+                    object_description[field.name] = BasicProtocolSchema(
+                        parsing_type=STRING,
+                        python_type=str,
+                        parser_tag="c",
+                        length=length,
+                        default_value=None if field.default == MISSING else field.default,
+                    )
+
+                # deal with bytes type
+                elif arg_type in [BYTES, bytes]:
+                    object_description[field.name] = BasicProtocolSchema(
+                        parsing_type=BYTES,
+                        python_type=bytes,
+                        parser_tag="c",
+                        length=length,
+                        default_value=None if field.default == MISSING else field.default,
+                    )
+
+                # deal with list type
+                elif get_origin(arg_type) == list:
+                    list_type_args = get_args(arg_type)
+
+                    if len(list_type_args) != 1:
+                        raise Exception(f"list must contain only one kind of data type (field: {field.name})")
+
+                    list_type = list_type_args[0]
+
+                    if list_type in ELEMENTARY_TYPE_LIST:
+                        object_description[field.name] = [
+                            BasicProtocolSchema(
+                                parsing_type=list_type,
+                                python_type=TYPE_TO_PYTYPE[list_type],
+                                parser_tag=TYPE_TO_TAG[list_type],
+                                length=TYPE_TO_LENGTH[list_type],
+                            )
+                        ] * length
+                    elif inspect.isclass(list_type) and is_dataclass(list_type):
+                        object_description[field.name] = [
+                            self.__build_data_description(list_type)
+                        ] * length
+                    else:
+                        raise Exception(f"unsupported list type: {list_type} (field: {field.name})")
+
+                else:
+                    raise Exception(f"unsupported annotated type: {arg_type} (field: {field.name})")
+
+                pass
+            elif field.type in [list, bytes, str, STRING, BYTES]:
+                raise Exception(f"cannot have unannotated list, string, or bytes type (length required, field: {field.name})")
+            else:
+                raise Exception(f"unsupported data type ({field.type}) on field {field.name}")
+
+        return object_description
 
     def feed(self, data: bytes) -> None:
-        pass
+        self.__data += data
 
     def __lshift__(self, data: bytes) -> Reader:
         self.feed(data)
         return self
 
-    def is_valid(self) -> bool:
-        pass
+    def is_complete(self) -> bool:
+        return self.__is_complete
 
-    def build(self):
+    def build(self) -> Any:
         pass
